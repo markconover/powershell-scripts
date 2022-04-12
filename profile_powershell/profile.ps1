b6b7ad6fc95fc7004e369d3844bb5e55614ebd3d
Mark Conover <mark.conover@fireeye.com>
2022-04-12 02:07:12 -0400

    Added some PowerShell notes to "profile.ps1"

--- profile_powershell/profile.ps1
+++ profile_powershell/profile.ps1
@@ -29,4 +29,60 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
 
+$env:PSModulePath -split ';'   
 
+Update-Help -Force -Verbose
+
+# Get-Help * -Parameter ComputerName
+# Get-Command -ParameterName ComputerName
+
+
+# Get-PSRepository | Format-List
+
+# Computer Info - PowerShell Commands
+# Write-Host $env:COMPUTERNAME
+# Get-CimClass -ClassName *bios*
+# Get-CimClass -ClassName Win32_Bios
+# Get-CimInstance -ClassName Win32_Bios
+# Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled = $true"
+# Get-CimInstance -Query "SELECT * FROM Win32_NetworkAdapterConfiguration WHERE DHCPEnabled = $true"
+
+# Get-Module -ListAvailable -All
+# Get-Module -All
+# Get-Module -ListAvailable | where { $_.path -match "System32" }
+
+# Get-Command -Module PowerShellGet | Format-Wide -Column 3
+# Get-Command -ParameterName Cimsession
+
+# Find-Module -Repository PSGallery
+# Find-Module -Name *pip*
+# Find-Module nx* | Format-Table Version, Name, Description
+# Find-Module nx* | Install-Module -Force
+# Find-Module -Tag 'Active Directory', 'ActiveDirectory', 'Active', 'Directory', 'AD'
+
+# Find-Script -Name *pip*
+
+# Get-ChildItem hklm:\software | Get-Member ps*
+
+# Get-Process | sort -Descending ws | select -First 3
+# Get-Process | where Handles -gt 1000
+
+# Get-CimInstance -ClassName Win32_OperatingSystem
+
+
+# Registry - PowerShell Commands
+# cd hklm:\software\microsoft\powershell
+# Get-ChildItem -Path Registry::
+# Get-ChildItem -Path registry::HKEY_CURRENT_CONFIG\System\CurrentControlSet\SERVICES\TSDDD\
+
+# Files - PowerShell Commands
+# Get-ChildItem -Path C:\ -Filter *.sys -Force
+
+# "netstat" - PowerShell Commands
+# netstat -n | select -Skip 4 | ConvertFrom-String -PropertyNames Blank, Protocol, LocalAddress, ForeignAddress, State | Select-Object Protocol, LocalAddress, ForeignAddress, State
+
+# "clipboard" - PowerShell Commands
+# Get-Clipboard
+
+# Processes - PowerShell Commands
+# Invoke-CimMethod -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine = 'calc.exe'}
