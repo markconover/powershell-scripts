80901457758e54297795f02b8f9c972db389a870
Mark Conover <mark.conover@mandiant.com>
2022-04-14 03:10:11 -0400

    Added new PowerShell script file "Get-Computer-Asset-Details.ps1" which gathers and outputs an asset details report (i.e., asset inventory report for all assets detected on the current network).

--- Get-Computer-Asset-Details.ps1
+++ Get-Computer-Asset-Details.ps1
@@ -14,9 +14,69 @@
     Writes out to screen and exports asset details to CSV file
 
 .PARAMETER
-    None
+    None. This script does not require any parameters.
 
+.INPUTS
+    None.
+
+.OUTPUTS
+    None.
+    
 .EXAMPLE
     The following will run and output a CSV file
     .\Get-Computer-Asset-Details.ps1
+
+.LINK
+    No links
+
+.NOTES
+    Additional information about the function or script.
 #>
+
+# function Get-SslCertificate {
+# function Get-AccessedFile{
+# function Get-ActiveComputer{
+# function Get-ActiveFile{
+# function Get-ActiveUser{
+# function Get-ChildItemLastAccessTime{
+# function Get-ChildItemLastWriteTime{
+# function Get-ComputerCurrentUser{
+# function Get-ComputerDriveInformation{
+# function Get-ComputerFailedLogonEvent{
+# function Get-ComputerInformation{
+# function Get-ComputerIPAddress{
+# function Get-ComputerLastBootUpTime{
+# function Get-ComputerLastLogonTime{
+# function Get-ComputerMemory{
+# function Get-ComputerModel{
+# function Get-ComputerOS{
+# function Get-ComputerPhysicalDiskInformation{
+# function Get-ComputerProcessor{
+# function Get-ComputerShareFolder{
+# function Get-ComputerSoftware{
+# function Get-ComputerSystemEvent{
+# function Get-CredentialExportToXML{
+# function Get-DHCPReservation{
+# function Get-DirectorySize{
+# function Get-DisabledComputer{
+# function Get-DisabledUser{
+# function Get-InactiveComputer{
+# function Get-InactiveFile{
+# function Get-InactiveUser{
+# function Get-ItemLastAccessTime{
+# function Get-ItemLastWriteTime{
+# function Get-LargeFile{
+# function Get-LockedOutUser{
+# function Get-LockedOutUserEvent{
+# function Get-OfflineComputer{
+# function Get-OnlineComputer{
+# function Get-OUComputer{
+# function Get-OUUser{
+# function Get-SubDirectorySize{
+# function Get-UserActiveLogon{
+# function Get-UserLastLogonTime{
+
+
+# ---------------------------------------------------------
+# Old Notes
+# ---------------------------------------------------------
